5123d30a7bcdc429c6c647eaf5c7213e413cb8e5
Juan Manuel Schillaci <jmschillaci@gmail.com>
2015-07-14 18:19:11 -0300

    Change databse name to 'notelis'

--- src/app/scripts/services/pouchdb.js
+++ src/app/scripts/services/pouchdb.js
@@ -10,5 +10,5 @@
 angular.module('applicationApp')
   .factory('pouchdb', function () {
       /*globals PouchDB*/
-      PouchDB.sync('notes', 'http://127.0.0.1:5984/notes');
-      return new PouchDB('notes');})
+      PouchDB.sync('notelis', 'http://127.0.0.1:5984/notes',{live:true});
+      return new PouchDB('notelis');})
